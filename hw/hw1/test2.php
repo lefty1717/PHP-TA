62fd1c72c81dd36c97e9b9ef5c93b5981e019a31
Finn Hsu <finn.hsu@ecloudvalley.com>
2023-10-12 14:25:29 +0800

    fix: hw1 comment

--- hw/hw1/test2.php
+++ hw/hw1/test2.php
@@ -1,22 +1,11 @@
 <?php
-    // echo var_dump($_GET);
-    $gender = 'male'? $_GET['gender'] : 'female';
-    $hight = $_GET['hight']>1 ? $_GET['hight']/100 : $_GET['hight'];
-    $weight = $_GET['weight'];
-
-    $bmi = $weight / ($hight * $hight);
-    if($gender == 'male'){
-        if($bmi > 25.5)
-            echo "BMI = $bmi, 肥胖";
-        elseif($bmi >= 23.2)
-            echo "BMI = $bmi, 過重";
-        elseif($bmi >= 17)
-            echo "BMI = $bmi, 正常";
-        else
-            echo "BMI = $bmi, 過輕";
-    if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['gender']) && isset($_GET['height']) && isset($_GET['weight'])){
+    if($_SERVER['REQUEST_METHOD'] == 'GET' && 
+        isset($_GET['gender']) && 
+        isset($_GET['height']) && 
+        isset($_GET['weight'])
+    ){
         $gender = $_GET['gender']=='male' ? '男' : '女';
-        $height = $_GET['height']>3 ? $_GET['height']/100 : $_GET['height'];
+        $height = $_GET['height']>5 ? $_GET['height']/100 : $_GET['height'];
         $weight = $_GET['weight'];
     
         $bmi = $weight / ($height * $height);
@@ -37,7 +26,7 @@
             elseif($bmi >= 22.8)
                 $comment = '過重';
             elseif($bmi >= 17)
-                 $comment = '正常';
+                $comment = '正常';
             else
                 $comment = '過輕';
         }
@@ -45,17 +34,10 @@
         echo "身高: $height m<br>";
         echo "體重: $weight kg<br>";
         echo "BMI: $bmi<br>";
+        echo "評語: $comment<br>";
         echo "<a href='test1.html'>返回</a>";
     }
     else{
-        if($bmi > 25.6)
-            echo "BMI = $bmi, 肥胖";
-        elseif($bmi >= 22.8)
-            echo "BMI = $bmi, 過重";
-        elseif($bmi >= 17)
-            echo "BMI = $bmi, 正常";
-        else
-            echo "BMI = $bmi, 過輕";
         echo '請輸入完整資料';
         header('refresh: 2; url=test1.html');
     }
