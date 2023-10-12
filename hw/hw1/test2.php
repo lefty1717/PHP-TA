f987bcc333bf7638074165e5ff876647c3ed5e20
Finn Hsu <hank1717@gamil.com>
2023-10-12 14:10:19 +0800

    feat: add hw1 check empty

--- hw/hw1/test2.php
+++ hw/hw1/test2.php
@@ -1,28 +1,39 @@
 <?php
-    // echo var_dump($_GET);
-    $gender = 'male'? $_GET['gender'] : 'female';
-    $hight = $_GET['hight']>1 ? $_GET['hight']/100 : $_GET['hight'];
-    $weight = $_GET['weight'];
+    if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['gender']) && isset($_GET['height']) && isset($_GET['weight'])){
+        $gender = $_GET['gender']=='male' ? '男' : '女';
+        $height = $_GET['height']>3 ? $_GET['height']/100 : $_GET['height'];
+        $weight = $_GET['weight'];
+    
+        $bmi = $weight / ($height * $height);
 
-    $bmi = $weight / ($hight * $hight);
-    if($gender == 'male'){
-        if($bmi > 25.5)
-            echo "BMI = $bmi, 肥胖";
-        elseif($bmi > 23.2)
-            echo "BMI = $bmi, 過重";
-        elseif($bmi > 17)
-            echo "BMI = $bmi, 正常";
-        else
-            echo "BMI = $bmi, 過輕";
+        if($gender == 'male'){
+            if($bmi > 25.5)
+                $comment = '肥胖';
+            elseif($bmi >= 23.2)
+                $comment = '過重';
+            elseif($bmi >= 17)
+                $comment = '正常';
+            else
+                $comment = '過輕';
+        }
+        else{
+            if($bmi > 25.6)
+                $comment = '肥胖';
+            elseif($bmi >= 22.8)
+                $comment = '過重';
+            elseif($bmi >= 17)
+                 $comment = '正常';
+            else
+                $comment = '過輕';
+        }
+        echo "性別: $gender<br>";
+        echo "身高: $height m<br>";
+        echo "體重: $weight kg<br>";
+        echo "BMI: $bmi<br>";
+        echo "<a href='test1.html'>返回</a>";
     }
     else{
-        if($bmi > 25.6)
-            echo "BMI = $bmi, 肥胖";
-        elseif($bmi > 22.8)
-            echo "BMI = $bmi, 過重";
-        elseif($bmi > 17)
-            echo "BMI = $bmi, 正常";
-        else
-            echo "BMI = $bmi, 過輕";
+        echo '請輸入完整資料';
+        header('refresh: 2; url=test1.html');
     }
 ?>
