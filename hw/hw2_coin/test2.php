20ae3d49356a404cbd74b037ba35a8a144250c6d
Finn Hsu <finn.hsu@ecloudvalley.com>
2023-11-16 14:51:35 +0800

    fix: coins true=1

--- hw/hw2_coin/test2.php
+++ hw/hw2_coin/test2.php
@@ -1,28 +1,28 @@
 <?php
-    $coin = $_GET['coin'];
-    $fifty = $_GET['fifty']=='true' ? true : false;
-    $ten = $_GET['ten']=='true' ? true : false;
-    $five = $_GET['five']=='true' ? true : false;
-    $one = $_GET['one']=='true' ? true : false;
+    // $coin = $_GET['coin'];
+    // $fifty = $_GET['fifty']=='true' ? true : false;
+    // $ten = $_GET['ten']=='true' ? true : false;
+    // $five = $_GET['five']=='true' ? true : false;
+    // $one = $_GET['one']=='true' ? true : false;
 
-    $ans = ['50'=>0, '10'=>0, '5'=>0, '1'=>0];
+    // $ans = ['50'=>0, '10'=>0, '5'=>0, '1'=>0];
 
-    if($fifty){
-        $ans['50'] = intdiv($coin, 50);
-        $coin %= 50;
-    }
-    if($ten){
-        $ans['10'] = intdiv($coin, 10);
-        $coin %= 10;
-    }
-    if($five){
-        $ans['5'] = intdiv($coin, 5);
-        $coin %= 5;
-    }
-    $ans['1'] = $coin;
+    // if($fifty){
+    //     $ans['50'] = intdiv($coin, 50);
+    //     $coin %= 50;
+    // }
+    // if($ten){
+    //     $ans['10'] = intdiv($coin, 10);
+    //     $coin %= 10;
+    // }
+    // if($five){
+    //     $ans['5'] = intdiv($coin, 5);
+    //     $coin %= 5;
+    // }
+    // $ans['1'] = $coin;
 
-    foreach($ans as $type => $value)
-        echo "$type 元: $value 個<br>";
+    // foreach($ans as $type => $value)
+    //     echo "$type 元: $value 個<br>";
 ?>
 
 <br>
@@ -30,12 +30,8 @@
 <?php
     $coin = $_GET['coin'];
     $ans = ['50'=>0, '10'=>0, '5'=>0, '1'=>0];
-    $setting = ['50'=>false, '10'=>false, '5'=>false, '1'=>false];
-    foreach ($setting as $type=>$chose)
-        $setting[$type] = $_GET[$type]=='true' ?? false;
-
-    foreach($setting as $type=>$chose){
-        if($chose){
+    foreach($ans as $type=>$num){
+        if($_GET[$type]){
             $ans[$type] = intdiv($coin, $type);
             $coin %= $type;
         }
@@ -51,7 +47,7 @@
     $coin = (int)$_GET['coin'];
     $ans = ['50'=>0, '10'=>0, '5'=>0, '1'=>0];
     foreach ($ans as $type=>$chose){
-        $ans[$type] = $_GET[$type]=='true' ? intdiv($coin, $type) : '0';
+        $ans[$type] = $_GET[$type] ? intdiv($coin, $type) : '0';
         $coin = gettype($ans[$type]) == 'string' ? $coin : $coin%$type;
         echo "$type 元: $ans[$type] 個<br>";
     }
