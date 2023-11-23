bad31e7b117bd61614ef51126fa12489c3245c0b
Finn Hsu <finn.hsu@ecloudvalley.com>
2023-11-23 14:51:32 +0800

    hw2 while way

--- hw/hw2_coin/test2.php
+++ hw/hw2_coin/test2.php
@@ -44,11 +44,32 @@
 <br>
 
 <?php
-    // $coin = (int)$_GET['coin'];
+    // $coin = $_GET['coin'];
     // $ans = ['50'=>0, '10'=>0, '5'=>0, '1'=>0];
     // foreach($ans as $type=>$chose){
-    //     $ans[$type] = $_GET[$type] ? intdiv($coin, $type) : '0';
+    //     $ans[$type] = $_GET[$type] ? floor($coin / $type) : '0';
     //     $coin = gettype($ans[$type]) == 'string' ? $coin : $coin%$type;
     //     echo "$type 元: $ans[$type] 個<br>";
     // }
+?>
+
+
+<?php
+while ($change_amount > 0) {
+        if ($change_amount >= 50 && $change_50 == 'yes') {
+            $change_amount -= 50;
+            $change_50_count++;
+        } elseif ($change_amount >= 10 && $change_10 == 'yes') {
+            $change_amount -= 10;
+            $change_10_count++;
+        } elseif ($change_amount >= 5 && $change_5 == 'yes') {
+            $change_amount -= 5;
+            $change_5_count++;
+        } elseif ($change_amount >= 1) {
+            $change_amount -= 1;
+            $change_1_count++;
+        } else {
+            break;
+        }
+    }
 ?>
